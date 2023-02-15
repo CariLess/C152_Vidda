db2914e20ebda706c7e20db878e13ea6587f5308
CariLess <90604545+CariLess@users.noreply.github.com>
2023-02-15 16:27:48 -0600

    Add files via upload

---
+++ Move-Position.js
@@ -0,0 +1,15 @@
+AFRAME.registerComponent("move", {
+    schema: {
+      moveX: { type: "number", default: 1 },
+    },
+  
+    tick: function () {
+     this.data.moveY = this.data.moveY + 0.01;
+      
+      var pos = this.el.getAttribute("position");
+  
+      pos.y = this.data.moveY;
+  
+      this.el.setAttribute("position", {x: pos.x, y: pos.y, z: pos.z});
+    }
+  });
